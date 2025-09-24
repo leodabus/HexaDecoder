abeee143f0b67d98544f901091bd89e881d8266c
leodabus <leodabus@gmail.com>
2025-09-24 03:51:12 -0300

    Initial commit

--- Sources/HexaDecoder/HexaDecoder.swift
+++ Sources/HexaDecoder/HexaDecoder.swift
@@ -1,2 +1,50 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import protocol Foundation.DataProtocol
+
+extension String {
+    enum HexaDecodingError: Error {
+        case invalidCharacter(Character), oddNumberOfCharacters
+    }
+}
+
+extension Collection {
+    func unfoldSubSequences(limitedTo maxLength: Int) -> UnfoldSequence<SubSequence,Index> {
+        sequence(state: startIndex) { lowerBound in
+            guard lowerBound < endIndex else { return nil }
+            let upperBound = index(
+                lowerBound,
+                offsetBy: maxLength,
+                limitedBy: endIndex
+            ) ?? endIndex
+            defer { lowerBound = upperBound }
+            return self[lowerBound..<upperBound]
+        }
+    }
+}
+
+extension StringProtocol {
+    func hexa<D>() throws -> D where D: DataProtocol & RangeReplaceableCollection {
+        try .init(self)
+    }
+}
+
+extension DataProtocol where Self: RangeReplaceableCollection {
+    init<S: StringProtocol>(_ hexa: S) throws {
+        guard hexa.count.isMultiple(of: 2) else {
+            throw String.HexaDecodingError.oddNumberOfCharacters
+        }
+        self = .init()
+        reserveCapacity(hexa.utf8.count/2)
+        for pair in hexa.unfoldSubSequences(limitedTo: 2) {
+            guard let byte = UInt8(pair, radix: 16) else {
+                for character in pair where !character.isHexDigit {
+                    throw String.HexaDecodingError.invalidCharacter(character)
+                }
+                continue
+            }
+            append(byte)
+        }
+    }
+}
